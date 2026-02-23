70358082f723a5c3e454236c08bada6960204af3
Boaz Bokek <boazbokek2@gmail.com>
2026-02-23 18:31:33 +0000

    daily weather table created

--- models/dim_date.sql
+++ models/dim_date.sql
@@ -5,12 +5,8 @@ to_timestamp(started_at) started_at
 ,date(to_timestamp(started_at)) date_started_at
 ,hour(to_timestamp(started_at)) hour_started_at
 ,dayname(to_timestamp(started_at)) day_started_at
-,case when dayname(to_timestamp(started_at)) in ('Sat','Fri') then 'Weekend' else 'Business day' end day_type
-,case when month(to_timestamp(started_at)) in (12,1,2) then 'Winter'
-      when month(to_timestamp(started_at)) in (3,4,5) then 'Spring'
-      when month(to_timestamp(started_at)) in (6,7,8) then 'Summer'
-      else 'Autom' end station_of_year 
-
+,{{day_type('started_at')}} day_type
+,{{get_season('started_at')}}
 from {{ source('demo', 'bike') }}    
 where started_at != 'started_at'
 )
