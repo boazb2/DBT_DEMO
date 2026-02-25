4cfb2031959f112715102c5ddc2aefc1b63e70fa
Boaz Bokek <boazbokek2@gmail.com>
2026-02-25 17:19:44 +0000

    dim station created

--- models/dim_date.sql
+++ models/dim_date.sql
@@ -6,7 +6,7 @@ to_timestamp(started_at) started_at
 ,hour(to_timestamp(started_at)) hour_started_at
 ,dayname(to_timestamp(started_at)) day_started_at
 ,{{day_type('started_at')}} day_type
-,{{get_season('started_at')}}
+,{{get_season('started_at')}} season
 from {{ source('demo', 'bike') }}    
 where started_at != 'started_at'
 )
